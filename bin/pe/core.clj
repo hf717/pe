f534940d2a9a04466edbc1aa91b2766a3a3000d9
hf717 <hf717@yandex.ru>
2014-08-10 19:46:51 +0100

    add | initial setup of the project

---
+++ bin/pe/core.clj
@@ -0,0 +1,6 @@
+(ns pe.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ bin/pe/core_test.clj
@@ -0,0 +1,7 @@
+(ns pe.core-test
+  (:use clojure.test
+        pe.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
